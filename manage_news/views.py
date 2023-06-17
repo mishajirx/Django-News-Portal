7bc0efebc93f4894a59b7be3b5e160c5cb16afa3
Mike <mishalolhopengok@gmail.com>
2023-06-18 02:40:54 +0300

    Finished login.html

--- manage_news/views.py
+++ manage_news/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView, FormView, View
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
 from .models import News
+
 
 # Create your views here.
 
@@ -15,3 +18,23 @@ class MainView(TemplateView):
             return render(request, self.template_name, ctx)
         else:
             return render(request, self.template_name, {})
+
+
+class LoginView(FormView):
+    form_class = AuthenticationForm
+    success_url = '/news/'
+    template_name = 'login.html'
+
+    def form_valid(self, form):
+        self.user = form.get_user()
+        login(self.request, self.user)
+        return super(LoginView, self).form_valid(form)
+
+    def form_invalid(self, form):
+        return super(LoginView, self).form_invalid(form)
+
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect('/news/')
