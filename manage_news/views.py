58c5b11e8f2339210e7e9524abd36aa39d3b3fac
Mike <mishalolhopengok@gmail.com>
2023-06-18 02:00:40 +0300

    Added basic views Added login.html

--- manage_news/views.py
+++ manage_news/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import News
 
 # Create your views here.
+
+
+class MainView(TemplateView):
+    template_name = 'base.html'
+
+    def get(self, request):
+        if request.user.is_authenticated:
+            news = News.objects.filter(author=request.user)
+            ctx = {'news': news}
+            return render(request, self.template_name, ctx)
+        else:
+            return render(request, self.template_name, {})
--- news_portal/urls.py
+++ news_portal/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+import manage_news
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('news/', include('manage_news.urls'))
 ]
